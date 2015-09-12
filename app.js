4e1db6469ad5ea45dc3e9ef9ca63c9056b151816
Dugan Knoll <ddknoll@gmail.com>
2015-09-11 17:49:56 -0700

    Updates

--- app.js
+++ app.js
@@ -45486,9 +45486,19 @@ SVGeneration.GraphicList = React.createClass({displayName: "GraphicList",
       graphics: []
     };
   },
+  componentDidMount: function() {
+    $(window).resize(function(){
+      this.setState({ height: ($(window).height()-64) });
+    });
+  },
+  getInitialState: function() {
+    return {
+      height: ($(window).height()-64) 
+    };
+  },
   render: function() {
     return (
-      React.createElement("div", {className: this.props.active ? "active menu-dropdown": "menu-dropdown"}, 
+      React.createElement("div", {style: {height: this.state.height}, className: this.props.active ? "active menu-dropdown": "menu-dropdown"}, 
         React.createElement("ul", null, 
           
             this.props.graphics.map(function(img){
@@ -45573,7 +45583,8 @@ SVGeneration.Editor = React.createClass({displayName: "Editor",
       image: false,
       script: false,
       windowHeight: $(window).height(),
-      currentParameters: []
+      currentParameters: [],
+      showMore: false,
     };
   },
   componentDidMount: function() {
@@ -45587,13 +45598,15 @@ SVGeneration.Editor = React.createClass({displayName: "Editor",
   dataURI: function(){
     return "url(data:image/svg+xml;base64," + window.btoa(this.generate()) + ")";
   },
+  getSvgCss: function(){
+    var style = "background-color: #"+(this.state.image.bgColor ? this.state.currentParameters[this.state.image.bgColor] : 'ffffff')+";\n";
+    style = style + (!!this.state.image.cssRules ? this.state.image.cssRules+"\n" : '');
+    return style + "background-image:"+this.dataURI()+";" 
+  },
   renderSVG: function(){
     var node = document.createElement('div');
     node.setAttribute('class', 'svg-target');
-    var style = "background-color: #"+(this.state.image.bgColor ? this.state.currentParameters[this.state.image.bgColor] : 'ffffff')+';';
-    style = style + (!!this.state.image.cssRules ? this.state.image.cssRules : '');
-    style = style + " background-image:"+this.dataURI()+";"
-    node.setAttribute('style',style);
+    node.setAttribute('style',this.getSvgCss());
     var tmp = document.createElement("div");
     tmp.appendChild(node);
     return tmp.innerHTML;
@@ -45624,10 +45637,16 @@ SVGeneration.Editor = React.createClass({displayName: "Editor",
   navigate: function(active){
     this.setState({active: active})
   },
+  setScript: function(value){
+    this.setState({script: value});
+  },
   setParam: function(key, value){
     var newParams = this.state.currentParameters;
     newParams[key] = value;
     this.setState({currentParameters: newParams});
+  },
+  toggleMore: function(){
+    this.setState({showMore: !this.state.showMore});
   },
   generate: function(){
     var params = [];
@@ -45641,7 +45660,6 @@ SVGeneration.Editor = React.createClass({displayName: "Editor",
                   + this.state.script
                   + "\n return generate(params); \n"
                   + '})();'
-    console.log(eval(script));
     return eval(script);
   },
   render: function() {
@@ -45655,10 +45673,11 @@ SVGeneration.Editor = React.createClass({displayName: "Editor",
         React.createElement("div", null, 
           React.createElement("div", {className: "description"}, 
             React.createElement("h1", {className: "image-title"}, this.state.image.title), 
-            React.createElement("p", {className: "image-description", dangerouslySetInnerHTML: {__html:this.state.image.instructions}})
+            React.createElement("a", {onClick: this.toggleMore, className: "show-more"}, "Show ", this.state.showMore ? "Less" : "More"), 
+            React.createElement("p", {className:  this.state.showMore ? "image-description visible" : 'image-description', dangerouslySetInnerHTML: {__html:this.state.image.instructions}})
           ), 
           React.createElement(SVGeneration.TabBar, {navigate: this.navigate, active: this.state.active}), 
-          React.createElement(SVGeneration.Tabs, {setParam: this.setParam, image: this.state.image, active: this.state.active, style: {height: this.state.windowHeight-128}})
+          React.createElement(SVGeneration.Tabs, {setParam: this.setParam, currentParameters: this.state.currentParameters, image: this.state.image, active: this.state.active, getSvgCss: this.getSvgCss, script: this.state.script, setScript: this.setScript, style: {height: this.state.windowHeight-128}})
         )
       )
       var style = {
@@ -45709,11 +45728,11 @@ SVGeneration.Tabs = React.createClass({displayName: "Tabs",
   render: function(){
     var inner = '';
     if (this.props.active == 'Parameters') {
-      inner = React.createElement(SVGeneration.ParamsTab, {setParam: this.props.setParam, image: this.props.image})
+      inner = React.createElement(SVGeneration.ParamsTab, React.__spread({},   this.props))
     } else if (this.props.active == 'Source Code') {
-      inner = React.createElement(SVGeneration.SourceTab, {image: this.props.image})
+      inner = React.createElement(SVGeneration.SourceTab, React.__spread({},   this.props))
     } else {
-      inner = React.createElement(SVGeneration.ExportTab, {image: this.props.image})
+      inner = React.createElement(SVGeneration.ExportTab, React.__spread({},   this.props))
     }
     return (
       React.createElement("div", {style: this.props.style, className: "inner-tab"}, 
@@ -45725,16 +45744,14 @@ SVGeneration.Tabs = React.createClass({displayName: "Tabs",
 
 SVGeneration.ParamsTab = React.createClass({displayName: "ParamsTab",
   renderParam: function(param){
-    var props = {
-      param: param,
-      setParam: this.props.setParam
-    }
     if(param.type == 'int'){
-      return React.createElement(SVGeneration.IntegerSlider, React.__spread({},  props))
+      return React.createElement(SVGeneration.IntegerSlider, React.__spread({key: 'param-'+param.name},   this.props, {param: param}))
+    } if(param.type == 'percent'){
+      return React.createElement(SVGeneration.IntegerSlider, React.__spread({type: "percent", key: 'param-'+param.name},   this.props, {param: param}))
     } else if(param.type == 'color'){
-      return React.createElement(SVGeneration.ColorPicker, React.__spread({},  props))
+      return React.createElement(SVGeneration.ColorPicker, React.__spread({key: 'param-'+param.name},   this.props, {param: param}))
     } else {
-      return React.createElement("input", null)
+      return React.createElement("input", {key: 'param-'+param.name})
     }
   },
   render: function(){
@@ -45749,10 +45766,18 @@ SVGeneration.ParamsTab = React.createClass({displayName: "ParamsTab",
 });
 
 SVGeneration.SourceTab = React.createClass({displayName: "SourceTab",
+  componentDidMount: function() {
+    this.editor = ace.edit('ace-editor');
+    this.editor.setTheme("ace/theme/monokai");
+    this.editor.getSession().setMode("ace/mode/javascript");
+  },
+  editor: undefined,
   render: function(){
     return (
-      React.createElement("div", {className: ""}, 
-        "Source"
+      React.createElement("div", {className: "source-tab"}, 
+        React.createElement("div", {ref: "editor", id: "ace-editor"}, 
+          this.props.script
+        )
       )
     );
   }
@@ -45762,7 +45787,9 @@ SVGeneration.ExportTab = React.createClass({displayName: "ExportTab",
   render: function(){
     return (
       React.createElement("div", {className: ""}, 
-        "Export"
+        React.createElement("pre", null, 
+          this.props.getSvgCss()
+        )
       )
     );
   }
@@ -45778,24 +45805,26 @@ SVGeneration.IntegerSlider = React.createClass({displayName: "IntegerSlider",
     } else if (this.props.param.max - this.props.param.min < 100){
       step = 1;
     }
-    var options = {
+    $(React.findDOMNode(this.refs.slider)).slider({
       min: this.props.param.min,
       max: this.props.param.max,
       step: step,
-      value: this.props.param.default,
+      value: this.props.currentParameters[this.props.param.name],
       slide: function(event, ui){
         this.props.setParam(this.props.param.name, ui.value);
       }.bind(this),
-    };
-    console.log(options)
-    $(React.findDOMNode(this.refs.slider)).slider(options);
+    });
   },
   render: function(){
+    var value = this.props.currentParameters[this.props.param.name];
+    if(this.props.type && this.props.type.toLowerCase() == 'percent'){
+      value = value + "%";
+    }
     return (
       React.createElement("div", {className: "param-control"}, 
         React.createElement("label", {htmlFor: "param-" + this.props.param.name}, this.props.param.name), 
-        React.createElement("div", null, 
-          React.createElement("input", {id: "param-" + this.props.param.name, value: this.props.param.value}), 
+        React.createElement("div", {className: "input-control"}, 
+          React.createElement("input", {id: "param-" + this.props.param.name, value: value}), 
           React.createElement("div", {className: "slider", ref: "slider"})
         )
       )
@@ -45804,13 +45833,26 @@ SVGeneration.IntegerSlider = React.createClass({displayName: "IntegerSlider",
 });
 
 SVGeneration.ColorPicker = React.createClass({displayName: "ColorPicker",
+  componentDidMount: function() {
+    $(React.findDOMNode(this.refs.colorPicker)).spectrum({
+      showInput: true,
+      allowEmpty: false,
+      preferredFormat:  "hex",
+      change: function(color) {
+        this.props.setParam(this.props.param.name, color.toHexString().replace('#',''));
+      }.bind(this),
+      move: function(color) {
+        this.props.setParam(this.props.param.name, color.toHexString().replace('#',''));
+      }.bind(this)
+    });
+  },
   render: function(){
     return (
       React.createElement("div", {className: "param-control"}, 
-        React.createElement("label", {htmlFor: ""}, this.props.param.name), 
-        React.createElement("div", null, 
-          React.createElement("input", {id: this.props.param.name, value: this.props.param.value}), 
-          React.createElement("div", {ref: "color-picker"})
+        React.createElement("label", {htmlFor: "param-"+this.props.param.name}, this.props.param.name), 
+        React.createElement("div", {className: "input-control"}, 
+          React.createElement("input", {ref: "colorPickerInput", id: "param-"+this.props.param.name, value: "#" + this.props.currentParameters[this.props.param.name]}), 
+          React.createElement("input", {ref: "colorPicker", id: "param-"+this.props.param.name, value: "#" + this.props.currentParameters[this.props.param.name]})
         )
       )
     )
